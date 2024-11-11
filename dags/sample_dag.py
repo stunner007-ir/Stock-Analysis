0856b7c1872dcca7b210e39329e152c18249007e
stunner007-ir <ishu9693raj@gmail.com>
2024-11-11 12:33:20 +0530

    u7

---
+++ dags/sample_dag.py
@@ -0,0 +1,16 @@
+# dags/sample_dag.py
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+}
+
+with DAG('sample_dag', default_args=default_args, schedule_interval='@daily') as dag:
+    start = DummyOperator(task_id='start')
+    end = DummyOperator(task_id='end')
+
+    start >> end
